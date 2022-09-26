cf12ac64c2aedefbab0dad9e457973ec6f02352d
firdavs <firdavsolimjonov25@gmail.com>
2022-09-26 10:04:27 +0500

    wwww

--- src/main/java/ai/ecma/codingbat/payload/UserMe.java
+++ src/main/java/ai/ecma/codingbat/payload/UserMe.java
@@ -5,11 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
 
 @AllArgsConstructor
 @Getter
 @Setter
-public class UserMe {
+public class UserMe implements Serializable {
 
     @NotBlank
     String email;
